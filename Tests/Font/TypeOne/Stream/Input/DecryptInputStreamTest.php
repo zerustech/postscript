1bcba920e69482432087d77b1d317ff23019956c
Michael Lee <michael.lee@zerustech.com>
2016-09-07 22:21:42 +0800

    Add decrypt input stream

---
+++ Tests/Font/TypeOne/Stream/Input/DecryptInputStreamTest.php
@@ -0,0 +1,204 @@
+<?php
+
+/**
+ * This file is part of the ZerusTech package.
+ *
+ * (c) Michael Lee <michael.lee@zerustech.com>
+ *
+ * For full copyright and license information, please view the LICENSE file that
+ * was distributed with this source code.
+ */
+
+namespace ZerusTech\Component\Postscript\Tests\Font\TypeOne\Stream\Input;
+
+use ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input\DecryptInputStream;
+use ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input\AsciiHexadecimalToBinaryInputStream;
+use ZerusTech\Component\Postscript\Font\TypeOne\Stream\Output\AsciiHexadecimalToBinaryOutputStream;
+use ZerusTech\Component\IO\Stream\Input\StringInputStream;
+use ZerusTech\Component\IO\Stream\Input\FileInputStream;
+use ZerusTech\Component\IO\Stream\Input\InputStreamInterface;
+use ZerusTech\Component\IO\Stream\Input\LineInputStream;
+use ZerusTech\Component\IO\Stream\Output\StringOutputStream;
+
+/**
+ * Test case for abstract encryptor.
+ *
+ * @author Michael Lee <michael.lee@zerustech.com>
+ */
+class DecryptInputStreamTest extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        $this->ref = new \ReflectionClass('ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input\DecryptInputStream');
+
+        $this->r = $this->ref->getProperty('r');
+        $this->r->setAccessible(true);
+
+        $this->R = $this->ref->getProperty('R');
+        $this->R->setAccessible(true);
+
+        $this->c1 = $this->ref->getProperty('c1');
+        $this->c1->setAccessible(true);
+
+        $this->c2 = $this->ref->getProperty('c2');
+        $this->c2->setAccessible(true);
+
+        $this->n = $this->ref->getProperty('n');
+        $this->n->setAccessible(true);
+
+        $this->in = $this->ref->getProperty('in');
+        $this->in->setAccessible(true);
+
+        $this->input = $this->ref->getMethod('input');
+        $this->input->setAccessible(true);
+
+        $this->base = __DIR__.'/../../../../Fixtures/Font/TypeOne/';
+    }
+
+    public function tearDown()
+    {
+        $this->input = null;
+        $this->in = null;
+        $this->n = null;
+        $this->c2 = null;
+        $this->c1 = null;
+        $this->R = null;
+        $this->r = null;
+        $this->ref = null;
+    }
+
+    public function testConstructor()
+    {
+        $in = new StringInputStream('hello');
+        $stream = new DecryptInputStream($in, 4330, 4);
+
+        $this->assertEquals(4330, $this->R->getValue($stream));
+        $this->assertEquals(4330, $this->r->getValue($stream));
+        $this->assertEquals(4, $this->n->getValue($stream));
+        $this->assertEquals(52845, $this->c1->getValue($stream));
+        $this->assertEquals(22719, $this->c2->getValue($stream));
+        $this->assertSame($in, $this->in->getValue($stream));
+    }
+
+    /**
+     * @dataProvider getDataForTestInput
+     */
+    public function testInput($R, $n, $encrypted, $offset, $length, $expected, $count, $skipped, $available)
+    {
+        $encryptedInput = new AsciiHexadecimalToBinaryInputStream(new StringInputStream($encrypted));
+
+        $expectedInput = new AsciiHexadecimalToBinaryInputStream(new StringInputStream($expected));
+
+        $expectedInput->read($expectedBin, strlen($expected));
+
+        $stream = new DecryptInputStream($encryptedInput, $R, $n); 
+
+        $this->assertEquals($skipped, $stream->skip($offset));
+
+        $this->assertEquals($count, $this->input->invokeArgs($stream, [&$bytes, $length]));
+
+        $this->assertEquals($expectedBin, $bytes);
+
+        $this->assertEquals($available, $stream->available());
+    }
+
+    public function getDataForTestInput()
+    {
+        return [
+            [4330, 4, "10 BF 31 70 9A A9 E3 3D EE", 0, 26, "68 65 6c 6c 6F", 26, 0, 0],
+            [55665, 4, 'E9 8D 09 D7 6C E6 99 52 F0', 0, 26, '68 65 6C 6C 6F', 26, 0, 0],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataForTestInputWithEexecBinFile
+     */
+    public function testInputWithEexecBinFile($cipherFile, $plainFile, $length)
+    {
+        $cipherFile = $this->base.$cipherFile;
+
+        $plainFile = $this->base.$plainFile;
+
+        $input = new DecryptInputStream(new FileInputStream($cipherFile, 'rb'), 55665, 4);
+
+        $output = new StringOutputStream();
+
+        while (-1 !== $this->input->invokeArgs($input, [&$bytes, $length])) {
+
+            $output->write($bytes);
+        }
+
+        $this->assertEquals(file_get_contents($plainFile), $output->__toString());
+    }
+
+    public function getDataForTestInputWithEexecBinFile()
+    {
+        return [
+            ['eexec-block-encrypted-as-bin-001.txt', 'eexec-block-decrypted-001.txt', 1],
+            ['eexec-block-encrypted-as-bin-001.txt', 'eexec-block-decrypted-001.txt', 2],
+            ['eexec-block-encrypted-as-bin-001.txt', 'eexec-block-decrypted-001.txt', 32],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataForTestInputWithEexecHexFile
+     */
+    public function testInputWithEexecHexFile($cipherFile, $plainFile, $length)
+    {
+        $cipherFile = $this->base.$cipherFile;
+
+        $plainFile = $this->base.$plainFile;
+
+        $input = new DecryptInputStream(new AsciiHexadecimalToBinaryInputStream(new FileInputStream($cipherFile, 'rb')), 55665, 4);
+
+        $output = new StringOutputStream();
+
+        while (-1 !== ($this->input->invokeArgs($input, [&$bytes, $length]))) {
+
+            $output->write($bytes);
+        }
+
+        $this->assertEquals(file_get_contents($plainFile), $output->__toString());
+    }
+
+    public function getDataForTestInputWithEexecHexFile()
+    {
+        return [
+            ['eexec-block-encrypted-as-hex-001.txt', 'eexec-block-decrypted-001.txt', 1],
+            ['eexec-block-encrypted-as-hex-001.txt', 'eexec-block-decrypted-001.txt', 2],
+            ['eexec-block-encrypted-as-hex-001.txt', 'eexec-block-decrypted-001.txt', 32],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataForTestInputWithCharStringHexFile
+     */
+    public function testInputWithCharStringHexFile($cipherFile, $plainFile, $length)
+    {
+        $cipherFile = $this->base.$cipherFile;
+
+        $plainFile = $this->base.$plainFile;
+
+        $cipherInput = new AsciiHexadecimalToBinaryInputStream(new LineInputStream(new FileInputStream($cipherFile, 'rb')));
+
+        $plainInput = new AsciiHexadecimalToBinaryInputStream(new LineInputStream(new FileInputStream($plainFile, 'rb')));
+
+        while (-1 !== ($cipherInput->read($cipherBin, $length)) && -1 !== ($plainInput->read($plainBin, $length))) {
+
+            $stream = new DecryptInputStream(new StringInputStream($cipherBin), 4330, 4);
+
+            $this->input->invokeArgs($stream, [&$bytes, strlen($cipherBin)]);
+
+            $this->assertEquals($plainBin, $bytes);
+        }
+
+    }
+
+    public function getDataForTestInputWithCharStringHexFile()
+    {
+        return [
+            ['charstring-encrypted-as-hex-001.txt', 'charstring-decrypted-to-encoded-hex-001.txt', 4, 1],
+            ['charstring-encrypted-as-hex-001.txt', 'charstring-decrypted-to-encoded-hex-001.txt', 4, 32],
+        ];
+    }
+}
---
+++ src/Font/TypeOne/Stream/Input/DecryptInputStream.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * This file is part of the ZerusTech package.
+ *
+ * (c) Michael Lee <michael.lee@zerustech.com>
+ *
+ * For full copyright and license information, please view the LICENSE file that
+ * was distributed with this source code.
+ */
+
+namespace ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input;
+
+use ZerusTech\Component\IO\Stream\Input\InputStreamInterface;
+use ZerusTech\Component\IO\Stream\Input\FilterInputStream;
+
+/**
+ * This class reads and decrypts the eexec / charstring encrypted data, in
+ * binary format, from the subordinate stream.
+ *
+ * @author Michael Lee <michael.lee@zerustech.com>
+ */
+class DecryptInputStream extends FilterInputStream
+{
+    /**
+     * @var int The ``R`` value of a specific type of encryptor. This value does
+     * not change during the encryption / decryption process.
+     */
+    protected $R = 0;
+
+    /**
+     * @var int The next r value of current encryptor. This value changes during
+     * the encryption / decryption process.
+     */
+    protected $r = 0;
+
+    /**
+     * @var int The c1 value. This value does not change.
+     */
+    protected $c1 = 52845;
+
+    /**
+     * @var int The c2 value. This value does not change.
+     */
+    protected $c2 = 22719;
+
+    /**
+     * @var int The number of leading random numbers. This value does not change.
+     */
+    protected $n = 4;
+
+
+    /**
+     * @var int It keeps track of the number of random bytes skipped. The
+     * leading random bytes should be skipped from the decrypted text.
+     */
+    protected $skipped;
+
+    /**
+     * This method creates a new decrypt input stream.
+     *
+     * @param InputStreamInterface $in The subordinate input stream.
+     * @param int $R The R value.
+     * @param int $n The number of leading random numbers.
+     */
+    public function __construct(InputStreamInterface $in, $R, $n = 4)
+    {
+        parent::__construct($in);
+
+        $this->r = $this->R = $R;
+
+        $this->n = $n;
+
+        $this->skipped = 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * This method reads up to ``$length`` bytes ciphertext from the subordinate
+     * stream, decrypts the ciphertext into ``$bytes`` and returns the
+     * actual number of bytes read.
+     *
+     * @param string $bytes The buffer into which the plain text will be
+     * stored.
+     * @param int $length The number of bytes to be read from the input stream.
+     * @return int The actual number of bytes read from the ciphertext input stream, or -1 if eof.
+     */
+    protected function input(&$bytes, $length)
+    {
+        $bytes = '';
+
+        $count = parent::input($cipherText, $length);
+
+        for ($i = 0; $i < strlen($cipherText); $i++) {
+
+            $cipherByte = ord($cipherText[$i]);
+
+            $plainByte = ($cipherByte ^ ($this->r >> 8));
+
+            $this->r = (($cipherByte + $this->r) * $this->c1 + $this->c2) % 65536;
+
+            if ($this->skipped < $this->n) {
+
+                $this->skipped++;
+
+                continue;
+            }
+
+            $bytes .= chr($plainByte);
+        }
+
+        return $count;
+    }
+}
