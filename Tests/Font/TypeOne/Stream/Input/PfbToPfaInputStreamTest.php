a3b013fa3c7ff71544d62bdcd465d7594ed9031c
Michael Lee <michael.lee@zerustech.com>
2016-09-09 19:38:15 +0800

    Add pfb to pfa input stream

---
+++ Tests/Font/TypeOne/Stream/Input/PfbToPfaInputStreamTest.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * This file is part of the ZerusTech package.
+ *
+ * (c) Michael Lee <michael.lee@zerustech.com>
+ *
+ * For full copyright and license information, please view the LICENSE file that
+ * was distributed with this source code.
+ */
+
+namespace ZerusTech\Component\Postscript\Tests\Font\TypeOne\Stream\Input;
+
+use ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input\PfbToPfaInputStream;
+use ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input\AsciiHexadecimalToBinaryInputStream;
+use ZerusTech\Component\IO\Stream\Input\StringInputStream;
+use ZerusTech\Component\IO\Stream\Input\FileInputStream;
+use ZerusTech\Component\IO\Stream\Output\StringOutputStream;
+use ZerusTech\Component\IO\Exception;
+
+/**
+ * Test case for file ascii hexadecimal to binary input stream.
+ *
+ * @author Michael Lee <michael.lee@zerustech.com>
+ */
+class PfbToPfaInputStreamTest extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        $this->ref = new \ReflectionClass('ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input\PfbToPfaInputStream');
+
+        $this->buffer = $this->ref->getProperty('buffer');
+        $this->buffer->setAccessible(true);
+
+        $this->header = $this->ref->getProperty('header');
+        $this->header->setAccessible(true);
+
+        $this->ready = $this->ref->getProperty('ready');
+        $this->ready->setAccessible(true);
+
+        $this->offset = $this->ref->getProperty('offset');
+        $this->offset->setAccessible(true);
+
+        $this->input = $this->ref->getMethod('input');
+        $this->input->setAccessible(true);
+
+        $this->base = __DIR__.'/../../../../Fixtures/Font/TypeOne/';
+    }
+
+    public function tearDown()
+    {
+        $this->input = null;
+        $this->offset = null;
+        $this->ready = null;
+        $this->header = null;
+        $this->buffer = null;
+        $this->ref = null;
+        $this->base = null;
+    }
+
+    public function testConstructor()
+    {
+        $src = new FileInputStream($this->base.'NimbusRomanNo9L-Regular.pfb', 'rb');
+
+        $input = new PfbToPfaInputStream($src);
+
+        $this->assertEquals('', $this->buffer->getValue($input));
+        $this->assertNull($this->header->getValue($input));
+        $this->assertFalse($this->ready->getValue($input));
+        $this->assertEquals(0, $this->offset->getValue($input));
+    }
+
+    /**
+     * @dataProvider getDataForTestInput
+     */
+    public function testInput($convertToHex, $width, $pfb, $offset, $length, $expected, $count, $skipped, $available)
+    {
+        $pfbInput = new PfbToPfaInputStream(new AsciiHexadecimalToBinaryInputStream(new StringInputStream($pfb)), $convertToHex, $width);
+
+        $this->assertEquals($skipped, $pfbInput->skip($offset));
+
+        $this->assertEquals($count, $this->input->invokeArgs($pfbInput, [&$bytes, $length]));
+
+        $this->assertEquals($available, $pfbInput->available());
+
+        $this->assertEquals($expected, $bytes);
+    }
+
+    public function getDataForTestInput()
+    {
+        return [
+            [false, 3, "80 01 05 00 00 00 68 65 6C 6C 6F", 0, 5, "hello", 5, 0, 0],
+            [false, 3, "80 01 05 00 00 00 68 65 6C 6C 6F", 0, 3, "hel", 3, 0, 1],
+            [false, 3, "80 01 05 00 00 00 68 65 6C 6C 6F", 1, 2, "el", 2, 1, 1],
+            [false, 3, "80 01 05 00 00 00 68 65 6C 6C 6F", 5, 1, "", -1, 5, 0],
+            [true, 3, "80 02 05 00 00 00 68 65 6C 6C 6F", 0, 10, "68656C\n6C6F", 10, 0, 0],
+            [true, 3, "80 02 0A 00 00 00 68 65 6C 6C 6F 68 65 6C 6C 6F", 0, 20, "68656C\n6C6F68\n656C6C\n6F", 20, 0, 0],
+            [true, 3, "80 02 0A 00 00 00 68 65 6C 6C 6F 68 65 6C 6C 6F", 1, 5, "656C\n6C", 6, 2, 1],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataForTestInputWithFile
+     */
+    public function testInputWithFile($pfbFile, $pfaFile, $convertToHex, $length)
+    {
+        $pfbFile = $this->base.$pfbFile;
+
+        $pfaFile = $this->base.$pfaFile;
+
+        $pfbInput = new FileInputStream($pfbFile, 'rb');
+
+        $stream = new PfbToPfaInputStream($pfbInput, $convertToHex);
+
+        $output = new StringOutputStream();
+
+        while (-1 !== $this->input->invokeArgs($stream, [&$bytes, $length])) {
+
+            $output->write($bytes);
+        }
+
+        $this->assertEquals(file_get_contents($pfaFile), $output->__toString());
+    }
+
+    public function getDataForTestInputWithFile()
+    {
+        return [
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-hex.pfa', true, 1],
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-hex.pfa', true, 2],
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-hex.pfa', true, 6],
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-hex.pfa', true, 32],
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-bin.pfa', false, 1],
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-bin.pfa', false, 2],
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-bin.pfa', false, 6],
+            ['NimbusRomanNo9L-Regular.pfb','NimbusRomanNo9L-Regular-pfb-to-bin.pfa', false, 32],
+        ];
+    }
+}
---
+++ src/Font/TypeOne/Stream/Input/PfbToPfaInputStream.php
@@ -0,0 +1,342 @@
+<?php
+
+/**
+ * This file is part of the ZerusTech package.
+ *
+ * (c) Michael Lee <michael.lee@zerustech.com>
+ *
+ * For full copyright and license information, please view the LICENSE file that
+ * was distributed with this source code.
+ */
+
+namespace ZerusTech\Component\Postscript\Font\TypeOne\Stream\Input;
+
+use ZerusTech\Component\IO\Exception\IOException;
+use ZerusTech\Component\IO\Stream\Input\FilterInputStream;
+use ZerusTech\Component\IO\Stream\Input\StringInputStream;
+use ZerusTech\Component\IO\Stream\Input\InputStreamInterface;
+
+/**
+ * This class reads bytes in PFB format from the subordinate stream and converts
+ * the bytes into PFA format. The converted eexec block is stored in binary
+ * format, which is the default eexec format in PFB format.
+ *
+ * PFB is a binary format for PC fonts. The PFB file is conceptually divided
+ * into segments, each of which has a small header containing a "type" field and
+ * length information. There are three types of segments:
+ *
+ * - Type 1: ASCII text. It can be directly used without any decompression.
+ * - Type 2: Binary data that should be converted to hexadecimal format.
+ * - Type 3: EOF. This is a flag that indicates that the end of the data
+ * segement has been reached.
+ *
+ * {@link https://partners.adobe.com/public/developer/en/font/5040.Download_Fonts.pdf downloadable postscript fonts}
+ *
+ * @author Michael Lee <michael.lee@zerustech.com>
+ */
+class PfbToPfaInputStream extends FilterInputStream
+{
+    /**
+     * This constant represents data segment type 1.
+     */
+    const TYPE_ASCII = 1;
+
+    /**
+     * This constant represents data segment type 2.
+     */
+    const TYPE_BINARY = 2;
+
+    /**
+     * This constant represents data segment type 3.
+     */
+    const TYPE_EOF = 3;
+
+    /**
+     * This constant represents the magic number in PFB header.
+     */
+    const MAGIC_NUMBER = 0x80;
+
+    /**
+     * Before a header is recognized, this string severs as a buffer that
+     * contains up to 6 bytes for the next header.
+     *
+     * @var string The buffer to help recognize the next header.
+     */
+    private $buffer;
+
+    /**
+     * The PFB header information of the next PFB block. The structure of a PFB
+     * header is as follows:
+     *
+     * The data structure of the parsed header is as follows:
+     *     array(
+     *         'magic-number' => 0x80,
+     *         'type' => (1|2|3),
+     *         'length' => ...
+     *     );
+     *
+     * @var array The PFB header information.
+     */
+    private $header;
+
+    /**
+     * @var bool This is a boolean flag that indicates if the header of the next
+     * segment has been parsed successfully and it's ready to read the data now.
+     */
+    private $ready = false;
+
+    /**
+     * @var int This is the relative offset from the beginning of current data
+     * segment.
+     */
+    private $offset = 0;
+
+    /**
+     * @var bool This is a boolean that indicates whether to convert the eexec
+     * block into hexadecimal format.
+     */
+    private $convertToHex = true;
+
+    /**
+     * The maximum number of columns (hexadecimal pairs) in each line.
+     */
+    private $width = 32;
+
+    /**
+     * This method creates a new pfb to pfa input stream.
+     *
+     * @param InputStreamInterface $input The subordinate stream, from which,
+     * the PFB bytes will be read.
+     * @param bool Indicates whether to convert the eexec data block to ascii
+     * hexadecimal format, true by default.
+     */
+    public function __construct(InputStreamInterface $input, $convertToHex = true, $width = 32)
+    {
+        parent::__construct($input);
+
+        $this->buffer = '';
+
+        $this->header = null;
+
+        $this->ready = false;
+
+        $this->offset = 0;
+
+        $this->convertToHex = $convertToHex;
+
+        $this->width = $width;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Because it's impossible to predict the length of pfa bytes, this
+     * method returns 1 if the subordinate stream is still available, or 0
+     * otherwise.
+     */
+    public function available()
+    {
+        return parent::available() > 0 ? 1 : 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * This method keeps reading pfb bytes from the subordinate stream, parsing
+     * header of the next data segment and storing the bytes from the data
+     * segment into ``$bytes``, until ``$length`` number of data bytes have been
+     * stored. The parsed data is in pfa format.
+     *
+     * @return int The number of parsed pfa bytes, or -1 if EOF.
+     */
+    protected function input(&$bytes, $length)
+    {
+        $bytes = '';
+
+        $remaining = $length;
+
+        while ($remaining > 0) {
+
+            if (-1 === ($count = $this->parseHeader($length))) {
+
+                break;
+            }
+
+            if (false === $this->ready) {
+
+                continue;
+            }
+
+            if (-1 === ($count = $this->parseBlock($bytes, $remaining))) {
+
+                break;
+            }
+
+            $remaining -= $count;
+
+            $this->resetHeader();
+
+            if (-1 === ($count = $this->parseRemaining($bytes, $remaining))) {
+
+                break;
+            }
+
+            $remaining -= $count;
+        }
+
+        return (-1 === $count && $remaining === $length) ? -1 : $length - $remaining;
+    }
+
+    /**
+     * This method reads up to 6 bytes from current stream and parses PFB header
+     * information.
+     *
+     * @param int $length The requested number of bytes to read.
+     * @return int The actual number of bytes read, or -1 if eof.
+     * @throws IOException If failed to parse the header information.
+     */
+    private function parseHeader($length)
+    {
+        $count = 0;
+
+        // Header has already been parsed, returns 0
+        if (true === $this->ready) {
+
+            return $count;
+        }
+
+        // Reads, which is the least, either the number of missing bytes for
+        // passing a header, or the specified numbrer of bytes, from the
+        // subordinate stream.
+        if (min(6 - strlen($this->buffer), $length) > 0) {
+
+            $count = parent::input($bytes, min(6 - strlen($this->buffer), $length));
+        }
+
+        $buffer = ($this->buffer .= $bytes);
+
+        // If EOF has been reached, and there was no byte in the buffer, the
+        // buffer could still be empty now and count MUST be -1 in this case.
+        if (0 === strlen($buffer)) {
+
+            return $count;
+        }
+
+        if (strlen($buffer) >= 1 && self::MAGIC_NUMBER !== ord($buffer[0])) {
+
+            throw new IOException(sprintf("Failed to parse margic number."));
+        }
+
+        if (strlen($buffer) >= 2 && !in_array(ord($buffer[1]), [static::TYPE_ASCII, static::TYPE_BINARY, static::TYPE_EOF])) {
+
+            throw new IOException(sprintf("Failed to parse data segment type information."));
+        }
+
+        if (strlen($buffer) >= 2 && ord($buffer[1]) === self::TYPE_EOF) {
+
+            $this->header = [];
+            $this->header['magic-number'] = self::MAGIC_NUMBER;
+            $this->header['type'] = ord($buffer[1]);
+            $this->header['length'] = 0;
+            $this->offset = 0;
+            $this->ready = true;
+
+            $count = -1;
+
+        } else if (6 === strlen($buffer)) {
+
+            $this->header = [];
+            $this->header['magic-number'] = self::MAGIC_NUMBER;
+            $this->header['type'] = ord($buffer[1]);
+            $this->header['length'] = ord($buffer[2]) | ord($buffer[3]) << 8 | ord($buffer[4]) << 16 | ord($buffer[5]) << 24;
+            $this->offset = 0;
+            $this->ready = true;
+        }
+
+        return $count;
+    }
+
+    /**
+     * When all bytes of current data block has been read and converted, this
+     * method resets header, buffer as well as other variables to their initial
+     * values.
+     */
+    private function resetHeader()
+    {
+        if ($this->header['length'] === $this->offset) {
+
+            $this->ready = false;
+            $this->header = null;
+            $this->buffer = '';
+            $this->offset = 0;
+        }
+    }
+
+    /**
+     * This method reads and parses the byte data from the upcoming data block.
+     *
+     * @param string $bytes The buffer, into which, the bytes will be read and
+     * stored.
+     * @param int $length The requested number of bytes to read.
+     * @return int The actual number of bytes read, or -1 if EOF.
+     */
+    private function parseBlock(&$bytes, $length)
+    {
+
+        if (self::TYPE_BINARY === $this->header['type'] && true === $this->convertToHex) {
+
+            $length = (0 === $length % 2) ? $length / 2 : ($length + 1) / 2;
+        }
+
+        $remaining = $length;
+
+        if (0 === ($available = min($remaining, $this->header['length'] - $this->offset))) {
+
+            return 0;
+        }
+
+        if (-1 === ($count = parent::input($bytes, $available))) {
+
+            return -1;
+        }
+
+        $this->offset += $count;
+
+        if (self::TYPE_ASCII === $this->header['type']) {
+
+            $bytes = str_replace("\r", "\n", $bytes);
+        }
+
+        if (self::TYPE_BINARY === $this->header['type'] && true === $this->convertToHex) {
+
+            $bin = new BinaryToAsciiHexadecimalInputStream(new StringInputStream($bytes), ($this->offset - strlen($bytes)) % $this->width, true, $this->width);
+
+            $count = $bin->read($bytes, strlen($bytes) * 2);
+        }
+
+        $remaining -= $count;
+
+        return $length - $remaining;
+    }
+
+    private function parseRemaining(&$bytes, $length)
+    {
+        $remaining = $length;
+
+        if ($remaining <= 0) {
+
+            return 0;
+        }
+
+        if (-1 === ($count = $this->input($next, $remaining))) {
+
+            return -1;
+        }
+
+        $bytes .= $next;
+
+        $remaining -= $count;
+
+        return $length - $remaining;
+    }
+}
