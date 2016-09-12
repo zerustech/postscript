acc90f9d36b222ceba848af395f7ed049bcfe021
Michael Lee <michael.lee@zerustech.com>
2016-09-12 16:29:01 +0800

    Re-implemented bin to hex input stream.
    It now does not format the hex string. The hex string should be formated by the
    format input stream.

--- Tests/Font/TypeOne/Stream/Input/BinaryToAsciiHexadecimalInputStreamTest.php
+++ Tests/Font/TypeOne/Stream/Input/BinaryToAsciiHexadecimalInputStreamTest.php
@@ -34,61 +34,31 @@ class BinaryToAsciiHexadecimalInputStreamTest extends \PHPUnit_Framework_TestCas
         $this->in = $this->ref->getProperty('in');
         $this->in->setAccessible(true);
 
-        $this->column = $this->ref->getProperty('column');
-        $this->column->setAccessible(true);
-
-        $this->format = $this->ref->getProperty('format');
-        $this->format->setAccessible(true);
-
-        $this->width = $this->ref->getProperty('width');
-        $this->width->setAccessible(true);
-
         $this->base = __DIR__.'/../../../../Fixtures/Font/TypeOne/';
     }
 
     public function  tearDown()
     {
         $this->base = null;
-        $this->width = null;
-        $this->format = null;
-        $this->column = null;
         $this->input = null;
         $this->ref = null;
     }
 
-    /**
-     * @dataProvider getDataForTestConstructor
-     */
-    public function testConstructor($column, $format, $width)
+    public function testConstructor()
     {
         $in = new StringInputStream('');
 
-        $stream = new BinaryToAsciiHexadecimalInputStream($in, $column, $format, $width);
+        $stream = new BinaryToAsciiHexadecimalInputStream($in);
 
         $this->assertSame($in, $this->in->getValue($stream));
-
-        $this->assertEquals($column, $this->column->getValue($stream));
-
-        $this->assertEquals($format, $this->format->getValue($stream));
-
-        $this->assertEquals($width, $this->width->getValue($stream));
-    }
-
-    public function getDataForTestConstructor()
-    {
-        return [
-            [0, true, 32],
-            [0, false, 16],
-            [16, true, 32],
-        ];
     }
 
     /**
      * @dataProvider getDataForTestInput
      */
-    public function testInput($column, $format, $width, $bin, $offset, $length, $expected, $count, $skipped, $available)
+    public function testInput($bin, $offset, $length, $expected, $count, $skipped, $available)
     {
-        $stream = new BinaryToAsciiHexadecimalInputStream(new StringInputStream($bin), $column, $format, $width);
+        $stream = new BinaryToAsciiHexadecimalInputStream(new StringInputStream($bin));
 
         $this->assertEquals($skipped, $stream->skip($offset));
 
@@ -102,35 +72,26 @@ class BinaryToAsciiHexadecimalInputStreamTest extends \PHPUnit_Framework_TestCas
     public function getDataForTestInput()
     {
         return [
-            [0, false, 4, 'hello', 0, 1, '68', 2, 0, 8],
-            [0, false, 4, 'hello', 0, 2, '68', 2, 0, 8],
-            [0, false, 4, 'hello', 0, 3, '6865', 4, 0, 6],
-            [0, false, 4, 'hello', 0, 10, '68656C6C6F', 10, 0, 0],
-            [0, false, 4, 'hello', 0, 11, '68656C6C6F', 10, 0, 0],
-            [0, false, 4, 'hello', 1, 1, '65', 2, 2, 6],
-            [0, false, 4, 'hello', 2, 1, '65', 2, 2, 6],
-            [0, false, 4, 'hello', 2, 2, '65', 2, 2, 6],
-            [0, false, 4, 'hello', 8, 2, '6F', 2, 8, 0],
-            [0, false, 4, 'hello', 10, 1, '', -1, 10, 0],
-            [0, false, 4, 'hello', 10, 2, '', -1, 10, 0],
-            [0, true, 4, '0123012301230123', 0, 32, "30313233\n30313233\n30313233\n30313233\n", 32, 0, 0],
-            [0, true, 4, '0123012301230123', 0, 33, "30313233\n30313233\n30313233\n30313233\n", 32, 0, 0],
-            [0, true, 4, '0123012301230123', 0, 12, "30313233\n3031", 12, 0, 20],
-            [2, true, 4, '0123012301230123', 0, 12, "3031\n32333031\n", 12, 0, 20],
-            [0, true, 4, '0123012301230123', 4, 28, "3233\n30313233\n30313233\n30313233\n", 28, 4, 0],
-            [1, true, 4, '0123012301230123', 4, 28, "32\n33303132\n33303132\n33303132\n33", 28, 4, 0],
-            [1, true, 4, '0123012301230123', 3, 28, "32\n33303132\n33303132\n33303132\n33", 28, 4, 0],
-            [0, true, 4, '0123012301230123', 32, 2, '', -1, 32, 0],
-            [0, true, 4, '0123012301230123', 32, 1, '', -1, 32, 0],
+            ['hello', 0, 1, '68', 2, 0, 8],
+            ['hello', 0, 2, '68', 2, 0, 8],
+            ['hello', 0, 3, '6865', 4, 0, 6],
+            ['hello', 0, 10, '68656C6C6F', 10, 0, 0],
+            ['hello', 0, 11, '68656C6C6F', 10, 0, 0],
+            ['hello', 1, 1, '65', 2, 2, 6],
+            ['hello', 2, 1, '65', 2, 2, 6],
+            ['hello', 2, 2, '65', 2, 2, 6],
+            ['hello', 8, 2, '6F', 2, 8, 0],
+            ['hello', 10, 1, '', -1, 10, 0],
+            ['hello', 10, 2, '', -1, 10, 0],
         ];
     }
 
     /**
      * @dataProvider getDataForTestInputWithFile
      */
-    public function testInputWithFile($hexFile, $binFile, $length)
+    public function testInputWithFile($binFile, $expectedFile, $length)
     {
-        $hexFile = $this->base.$hexFile;
+        $expectedFile = $this->base.$expectedFile;
 
         $binFile = $this->base.$binFile;
 
@@ -143,15 +104,13 @@ class BinaryToAsciiHexadecimalInputStreamTest extends \PHPUnit_Framework_TestCas
             $out->write($bin);
         }
 
-        $this->assertEquals(file_get_contents($hexFile), $out->__toString());
+        $this->assertEquals(trim(file_get_contents($expectedFile)), $out->__toString());
     }
 
     public function getDataForTestInputWithFile()
     {
         return [
-            ['eexec-block-encrypted-as-hex-001.txt', 'eexec-block-encrypted-as-bin-001.txt', 1],
-            ['eexec-block-encrypted-as-hex-001.txt', 'eexec-block-encrypted-as-bin-001.txt', 2],
-            ['eexec-block-encrypted-as-hex-001.txt', 'eexec-block-encrypted-as-bin-001.txt', 32],
+            ['eexec-block-encrypted-as-bin-001.txt', 'eexec-block-encrypted-as-hex-without-format-001.txt', 1],
         ];
     }
 }
--- src/Font/TypeOne/Stream/Input/BinaryToAsciiHexadecimalInputStream.php
+++ src/Font/TypeOne/Stream/Input/BinaryToAsciiHexadecimalInputStream.php
@@ -18,50 +18,18 @@ use ZerusTech\Component\IO\Stream\Input\FilterInputStream;
  * This class reads binary data from the subordinate input stream and converts
  * the data to ascii hexadecimal format.
  *
- * Linefeed ("\n") characters will be inserted into the generated hexadecimal
- * text to get a fixed number of columns for all lines.
- *
- * Each pair of hexadecimal bytes represents a column, and there are up to 32
- * columns, which is 64 bytes, in a line, by default.
- *
  * @author Michael Lee <michael.lee@zerustech.com>
  */
 class BinaryToAsciiHexadecimalInputStream extends FilterInputStream
 {
     /**
-     * @var int The column index of the next hexadecimal pair.
-     */
-    private $column;
-
-    /**
-     * @var bool A boolean indicates whether to format the ascii hexadecimal
-     * data.
-     */
-    private $format;
-
-    /**
-     * The maximum number of columns (hexadecimal pairs) in each line.
-     */
-    private $width = 32;
-
-    /**
      * This method creates a new binary to ascii hexadecimal input stream.
      *
      * @param InputStreamInterface $in The subordinate input stream.
-     * @param int $column The initial column index.
-     * @param bool $format The boolean that indicates whether to format the
-     * ascii hexadecimal data.
-     * @param int $width The maximum number of columns in each line.
      */
-    public function __construct(InputStreamInterface $in, $column = 0, $format = true, $width = 32)
+    public function __construct(InputStreamInterface $in)
     {
         parent::__construct($in);
-
-        $this->column = $column;
-
-        $this->format = $format;
-
-        $this->width = $width;
     }
 
     /**
@@ -77,50 +45,30 @@ class BinaryToAsciiHexadecimalInputStream extends FilterInputStream
      *
      * This method keeps reading bytes from the subordinate input
      * stream and converting the data from binary format to ascii hexadecimal
-     * format, till ``$length`` hexadecimal bytes have been converted.
-     *
-     * NOTE: the ``$length`` argument is the number of hexadecimal bytes
-     * converted, not the number of bytes read from the subordinate stream.
+     * format, untill ``$length`` hexadecimal bytes have been converted.
      *
      * Since one binary byte is always converted into two hexadecimal bytes, the
      * ``$length`` argument should be an even number. If the ``$length``
      * argument is an odd number, the next lowest even number (``$length + 1``)
      * will be used.
      *
-     * Line feeds are not counted in the return value of this method.
-     *
      * @return int The number of hexadecimal bytes converted, or -1 if EOF.
      */
     protected function input(&$bytes, $length)
     {
-        $remaining = $length = (0 === $length % 2) ? $length : $length + 1;
+        $remaining = $length = 0 === $length % 2 ? $length : $length + 1;
 
         $bytes = '';
 
-        while ($remaining > 0) {
+        while ($remaining > 0 && -1 !== parent::input($bin, $remaining / 2)) {
 
-            $num = (int)$remaining/2;
+            $hex = strtoupper(bin2hex($bin));
 
-            if (-1 === ($count = parent::input($bin, $num))) {
+            $bytes .= $hex;
 
-                break;
-            }
-
-            for ($i = 0; $i < strlen($bin); $i++) {
-
-                $bytes .= strtoupper(bin2hex($bin[$i]));
-
-                $remaining -= 2;
-
-                $this->column = ($this->column + 1) % $this->width;
-
-                if (0 === $this->column && true === $this->format) {
-
-                    $bytes .= "\n";
-                }
-            }
+            $remaining -= strlen($hex);
         }
 
-        return (-1 === $count && $remaining === $length) ? -1 : $length - $remaining;
+        return $remaining === $length ? -1 : $length - $remaining;
     }
 }
